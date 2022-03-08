9ecb265788df4069fbfcf8c470975a7eb4c83822
David Hofmann <83290245+davidhawes@users.noreply.github.com>
2022-03-08 21:01:59 +0100

    selectAllDemo() Methode zum Testen der Connection zur Datenbank

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcDemo {
 
     public static void main(String[] args) {
         System.out.println("JDBC Demo");
+        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'David Hofmann', 'dhofmann@tsn.at'), (NULL, 'Max Mustermann', 'mmustermann@tsn.at');
+        selectAllDemo();
+    }
+
+    public static void selectAllDemo(){
+        System.out.println("Select DEMO mit JDBC");
+        String sqlSelectAllPersons = "SELECT * FROM `student`";
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "";
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung hergestellt!");
+        } catch (SQLException e){
+            System.out.println("Fehler beim Aufbau der Verbindung zur Datenbank!: " + e.getMessage());
+        }
     }
 }
