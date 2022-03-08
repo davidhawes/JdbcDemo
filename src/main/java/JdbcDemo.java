abc8a111fbb89e1cf29d91aaaf08bfe71064750c
David Hofmann <83290245+davidhawes@users.noreply.github.com>
2022-03-08 21:12:09 +0100

    selectAllDemo() Methode erweitert um SQL statements auszuführen (mit preparedStatement und ResultSet)

--- src/main/java/JdbcDemo.java
+++ src/main/java/JdbcDemo.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcDemo {
 
@@ -16,8 +14,17 @@ public class JdbcDemo {
         String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
         String user = "root";
         String pwd = "";
+
         try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
             System.out.println("Verbindung hergestellt!");
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlSelectAllPersons);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println("Student aus der DB: {ID} " + id + " {NAME} " + name + " {EMAIL} " + email);
+            }
         } catch (SQLException e){
             System.out.println("Fehler beim Aufbau der Verbindung zur Datenbank!: " + e.getMessage());
         }
